d11bce6a52f20c180e6bd8e71270bc995a24630b
maturity-source <happymaturity@gmail.com>
2019-12-18 08:42:13 +0100

    ;,,,
    jjjjjjjjjjjjj

---
+++ src/main/java/classTest.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+
+/**
+ * @author yolande.kegne
+ *
+ */
+public class classTest {
+	
+
+	    public classTest() {
+			super();
+			// TODO Auto-generated constructor stub
+		}
+	    
+	    public void m1() {
+	    	String s="";
+	    ArrayList<Object> list = new ArrayList<Object>();
+	    	list.add(1);
+	    	list=null;
+	    	list.add(2);
+	    	Object obj=getData();
+	    	System.out.println(obj.toString());
+	    }
+
+		public Object getData() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		
+
+		public static void main( String[] args )
+		
+	    {
+	        System.out.println( "Hello World!" );
+	        App app=new App();
+	        app.m1();
+	    }
+	}
+
+}
